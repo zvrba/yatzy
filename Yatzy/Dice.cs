5705773306af9de3a2b469e810e9e3cb27554220
Zeljko Vrba <zvrba@zvrba.net>
2014-11-13 19:17:51 +0100

    Initial import.

---
+++ Yatzy/Dice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yatzy
+{
+  sealed class Probability
+  {
+
+  }
+
+  class DiceState
+  {
+
+  }
+
+  class RollingDice
+  {
+  }
+}
