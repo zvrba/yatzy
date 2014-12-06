0c3701cc422d543baa65edd2b1e849a71d57eade
Zeljko Vrba <zvrba@zvrba.net>
2014-12-06 16:17:30 +0100

    Fixed order of arguments to Assert.AreEqual

--- Tests/DiceState.cs
+++ Tests/DiceState.cs
@@ -21,10 +21,12 @@ namespace Tests
 
     public void SetValues(int[] values) {
       Debug.Assert(values.Length == 5);
+      providedCounts = new int[7];
       for (int i = 0; i < 7; ++i)
-        this.counts[i] = 0;
+        providedCounts[i] = 0;
       for (int i = 0; i < 5; ++i)
-        ++this.counts[values[i]];
+        ++providedCounts[values[i]];
+      SetState();
     }
   }
 
@@ -36,56 +38,56 @@ namespace Tests
     [TestMethod]
     public void Values_Correctly_Synthesized() {
       dice.SetCounts(new int[] { 0, 2, 0, 0, 1, 1, 1 });
-      Assert.AreEqual(dice.Values[0], 1);
-      Assert.AreEqual(dice.Values[1], 1);
-      Assert.AreEqual(dice.Values[2], 4);
-      Assert.AreEqual(dice.Values[3], 5);
-      Assert.AreEqual(dice.Values[4], 6);
+      Assert.AreEqual(1, dice.Values[0]);
+      Assert.AreEqual(1, dice.Values[1]);
+      Assert.AreEqual(4, dice.Values[2]);
+      Assert.AreEqual(5, dice.Values[3]);
+      Assert.AreEqual(6, dice.Values[4]);
     }
 
     [TestMethod]
     public void Scores_Correctly_Calculated() {
       dice.SetValues(new int[] { 1, 3, 3, 3, 1 });
-      Assert.AreEqual(EnumeratingDice.Ones.CalculateScore(dice), 2);
-      Assert.AreEqual(EnumeratingDice.Threes.CalculateScore(dice), 9);
-      Assert.AreEqual(EnumeratingDice.OnePair.CalculateScore(dice), 2);
-      Assert.AreEqual(EnumeratingDice.ThreeOfAKind.CalculateScore(dice), 9);
-      Assert.AreEqual(EnumeratingDice.House.CalculateScore(dice), 11);
+      Assert.AreEqual(2, EnumeratingDice.Ones.CalculateScore(dice));
+      Assert.AreEqual(9, EnumeratingDice.Threes.CalculateScore(dice));
+      Assert.AreEqual(2, EnumeratingDice.OnePair.CalculateScore(dice));
+      Assert.AreEqual(9, EnumeratingDice.ThreeOfAKind.CalculateScore(dice));
+      Assert.AreEqual(11, EnumeratingDice.House.CalculateScore(dice));
 
       dice.SetValues(new int[] { 2, 2, 5, 2, 2 });
-      Assert.AreEqual(EnumeratingDice.Twos.CalculateScore(dice), 8);
-      Assert.AreEqual(EnumeratingDice.Fives.CalculateScore(dice), 5);
-      Assert.AreEqual(EnumeratingDice.FourOFAKind.CalculateScore(dice), 8);
-      Assert.AreEqual(EnumeratingDice.ThreeOfAKind.CalculateScore(dice), 6);
-      Assert.AreEqual(EnumeratingDice.TwoPairs.CalculateScore(dice), 0);
-      Assert.AreEqual(EnumeratingDice.Chance.CalculateScore(dice), 13);
+      Assert.AreEqual(8, EnumeratingDice.Twos.CalculateScore(dice));
+      Assert.AreEqual(5, EnumeratingDice.Fives.CalculateScore(dice));
+      Assert.AreEqual(8, EnumeratingDice.FourOFAKind.CalculateScore(dice));
+      Assert.AreEqual(6, EnumeratingDice.ThreeOfAKind.CalculateScore(dice));
+      Assert.AreEqual(0, EnumeratingDice.TwoPairs.CalculateScore(dice));
+      Assert.AreEqual(13, EnumeratingDice.Chance.CalculateScore(dice));
 
       dice.SetValues(new int[] { 6, 1, 6, 4, 4 });
-      Assert.AreEqual(EnumeratingDice.Sixes.CalculateScore(dice), 12);
-      Assert.AreEqual(EnumeratingDice.Fours.CalculateScore(dice), 8);
-      Assert.AreEqual(EnumeratingDice.TwoPairs.CalculateScore(dice), 20);
+      Assert.AreEqual(12, EnumeratingDice.Sixes.CalculateScore(dice));
+      Assert.AreEqual(8, EnumeratingDice.Fours.CalculateScore(dice));
+      Assert.AreEqual(20, EnumeratingDice.TwoPairs.CalculateScore(dice));
 
       dice.SetValues(new int[] { 1, 2, 3, 4, 5 });
-      Assert.AreEqual(EnumeratingDice.SmallStraight.CalculateScore(dice), 15);
-      Assert.AreEqual(EnumeratingDice.LargeStraight.CalculateScore(dice), 0);
+      Assert.AreEqual(15, EnumeratingDice.SmallStraight.CalculateScore(dice));
+      Assert.AreEqual(0, EnumeratingDice.LargeStraight.CalculateScore(dice));
 
       dice.SetValues(new int[] { 2, 3, 4, 5, 6 });
-      Assert.AreEqual(EnumeratingDice.SmallStraight.CalculateScore(dice), 0);
-      Assert.AreEqual(EnumeratingDice.LargeStraight.CalculateScore(dice), 20);
+      Assert.AreEqual(0, EnumeratingDice.SmallStraight.CalculateScore(dice));
+      Assert.AreEqual(20, EnumeratingDice.LargeStraight.CalculateScore(dice));
 
       dice.SetValues(new int[] { 3, 3, 3, 3, 3 });
-      Assert.AreEqual(EnumeratingDice.Yatzy.CalculateScore(dice), 50);
+      Assert.AreEqual(50, EnumeratingDice.Yatzy.CalculateScore(dice));
 
       dice.SetValues(new int[] { 1, 2, 3, 4, 6 });
-      Assert.AreEqual(EnumeratingDice.OnePair.CalculateScore(dice), 0);
-      Assert.AreEqual(EnumeratingDice.TwoPairs.CalculateScore(dice), 0);
-      Assert.AreEqual(EnumeratingDice.ThreeOfAKind.CalculateScore(dice), 0);
-      Assert.AreEqual(EnumeratingDice.FourOFAKind.CalculateScore(dice), 0);
-      Assert.AreEqual(EnumeratingDice.SmallStraight.CalculateScore(dice), 0);
-      Assert.AreEqual(EnumeratingDice.LargeStraight.CalculateScore(dice), 0);
-      Assert.AreEqual(EnumeratingDice.House.CalculateScore(dice), 0);
-      Assert.AreEqual(EnumeratingDice.Yatzy.CalculateScore(dice), 0);
-      Assert.AreEqual(EnumeratingDice.Chance.CalculateScore(dice), 16);
+      Assert.AreEqual(0, EnumeratingDice.OnePair.CalculateScore(dice));
+      Assert.AreEqual(0, EnumeratingDice.TwoPairs.CalculateScore(dice));
+      Assert.AreEqual(0, EnumeratingDice.ThreeOfAKind.CalculateScore(dice));
+      Assert.AreEqual(0, EnumeratingDice.FourOFAKind.CalculateScore(dice));
+      Assert.AreEqual(0, EnumeratingDice.SmallStraight.CalculateScore(dice));
+      Assert.AreEqual(0, EnumeratingDice.LargeStraight.CalculateScore(dice));
+      Assert.AreEqual(0, EnumeratingDice.House.CalculateScore(dice));
+      Assert.AreEqual(0, EnumeratingDice.Yatzy.CalculateScore(dice));
+      Assert.AreEqual(16, EnumeratingDice.Chance.CalculateScore(dice));
     }
   }
 }
