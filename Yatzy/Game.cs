bf97d7fd167f241451573c475424de180a8c5684
Zeljko Vrba <zvrba@zvrba.net>
2014-12-13 17:11:00 +0100

    WIP.

--- Yatzy/Game.cs
+++ Yatzy/Game.cs
@@ -1,56 +1,66 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Yatzy
 {
-  abstract class AbstractGame
+  abstract class AbstractRuleGame
   {
-    public ReadOnlyCollection<int> Scores { get { return roScores; } }
-
-    protected readonly RollingDice dice;
-    protected readonly int[] scores;
-    protected readonly DiceStateComparer[] evaluators;
+    private readonly RollingDice dice;
+    private readonly int[] scores = new int[EnumeratingDice.Instances.Count];
     private readonly ReadOnlyCollection<int> roScores;
+    private int bonus;
 
-    protected AbstractGame() {
+    protected AbstractRuleGame(int seed) {
+      seed = (seed+1) * 1711; // Ensure not zero
+      dice = new RollingDice(seed);
       roScores = Array.AsReadOnly(scores);
+    }
+
+    public ReadOnlyCollection<int> Scores {
+      get { return roScores; }
+    }
+
+    public int Bonus {
+      get { return bonus; }
     }
 
     public void Play() {
       ResetScores();
-      for (int round = 0; round < evaluators.Length; ++round)
+      for (int round = 0; round < EnumeratingDice.Instances.Count; ++round)
         PlayRound(round);
+      SetBonus();
     }
 
-    protected AbstractGame(int seed, DiceStateComparer[] evaluators) {
-      seed = (seed+1) * 1711; // Ensure not zero
-      this.dice = new RollingDice(seed);
-      this.evaluators = evaluators;
-      this.scores = new int[evaluators.Length];
-    }
-
-    protected abstract int ChooseEvaluator(int throwsLeft);
+    protected abstract int ChooseTarget(int throwsLeft);
 
     private void ResetScores() {
       for (int i = 0; i < scores.Length; ++i)
         scores[i] = -1;
     }
 
+    private void SetBonus() {
+      if (scores.Take(6).Sum() >= 63)
+        bonus = 50;
+      else
+        bonus = 0;
+    }
+
     private void PlayRound(int round) {
       int de;
 
       dice.Roll();
-      de = ChooseEvaluator(2);
+      de = ChooseTarget(2);
       Debug.Assert(scores[de] == -1, "already used evaluator");
 
       dice.Roll(evaluators[de].DiceToHold);
-      de = ChooseEvaluator(1);
+      de = ChooseTarget(1);
       Debug.Assert(scores[de] == -1, "already used evaluator");
 
       dice.Roll(evaluators[de].DiceToHold);
-      de = ChooseEvaluator(0);
+      de = ChooseTarget(0);
       Debug.Assert(scores[de] == -1, "already used evaluator");
 
       // scores[de] = evaluators[de].Score;
